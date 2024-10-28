e50c1e3e1a86506c3f74f24fc03c1e2b39f03819
Siddharth292004 <sidharth9509@gmail.com>
2024-10-28 21:48:39 +0530

    update

---
+++ CPROGRAM/student.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the percentage : ");
+    scanf("%d",&n);
+    
+    if(n>=90 && n<=100)
+    printf(" grade  is A");
+    else if(n>=80 && n<90)
+    printf("grade is B ");
+    else if(n>=70 && n<80)
+    printf("grad is C ");
+    else if(n>=60 && n<70)
+    printf("grade is D ");
+    else {
+    printf("fail");
+    }
+    return 0;
+
+}
