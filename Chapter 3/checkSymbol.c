578e9e45fb3e1e662721341d26d9c23bac7b5f59
Siddharth292004 <sidharth9509@gmail.com>
2025-06-18 21:20:36 +0530

    modifie code

--- Chapter 3/checkSymbol.c
+++ Chapter 3/checkSymbol.c
@@ -6,24 +6,25 @@ The following table shows the range of ASCII values for various
 characters
 
 */
-
 #include<stdio.h>
 
-int main(){
+int main() {
     char ch;
-    printf("Enter the character from the keyword");
-    scanf("%d",&ch);
+    printf("Enter a character: ");
+    scanf(" %c", &ch);  // Notice the space before %c to consume any whitespace
 
-    if(ch >= 65 && ch <=90){
-        printf("The character is an uppercase letter\n");
+    if(ch >= 65 && ch <= 90) {
+        printf("The character is an uppercase letter.\n");
     }
-    else if (ch >= 97 && ch <= 122){
-        printf("The character is an lowercase letter\n");
+    else if(ch >= 97 && ch <= 122) {
+        printf("The character is a lowercase letter.\n");
     }
-    else if (ch >=48 && ch <= 57 ){
-        printf("The character is a digit\n");
+    else if(ch >= 48 && ch <= 57) {
+        printf("The character is a digit.\n");
     }
-    else if (ch >=0 && ch <48)|| (ch = 57 && ch <65) || (ch >90 || 97) ||ch >122){
-        printf("The character is a special symbol\n");
+    else {
+        printf("The character is a special symbol.\n");
     }
+
+    return 0;
 }
